b5a725ef156877a35050861fda563ff072107c7a
nicsxr <nikasxirtla@hotmail.com>
2020-10-13 00:22:55 +0400

    built

---
+++ Server/main.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+const cors = require('cors')
+const ffmpeg = require('fluent-ffmpeg')
+const bodyParser = require('body-parser');
+
+const port = 80
+app.use(express.static('public'))
+app.use(cors())
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.listen(port,() => {
+    console.log(`Example app listening at Port: ${port}`)
+})
+
+
+app.get('/', (req, res) => {
+    res.sendFile('Public/index.html')
+})
+
+app.get('/download', (req, res) =>{
+    link = req.query.url
+    startTime = parseFloat(req.query.startTime)
+    duration = parseFloat(req.query.duration)
+    title = req.query.title
+    console.log(startTime)
+    res.header('Content-Disposition',  `attachment; filename="${title}"-${startTime}-${startTime+duration}.mp4`)
+    ffmpeg().input(ytdl(link, {
+                        format: 'mp4',
+                    })).on('Error', (err) => console.log(err))
+                    //OUTPUT STREAM OPTIONS
+                    .seekInput(startTime)
+                    .duration(duration)
+                    .videoCodec('libx264')
+                    .format('avi')
+                    .on('error', (err) => console.log(err))
+                    .pipe(res);
+})
